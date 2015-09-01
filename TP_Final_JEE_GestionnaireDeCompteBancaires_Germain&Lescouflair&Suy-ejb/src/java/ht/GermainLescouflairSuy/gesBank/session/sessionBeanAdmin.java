5afa7268cc501b1c9baedaaaddd8c3ff2acdb1d8
grlescou <gaetanrobert.lescouflair@esih.edu>
2015-09-01 11:11:52 -0400

    Adding Packages

---
+++ TP_Final_JEE_GestionnaireDeCompteBancaires_Germain&Lescouflair&Suy-ejb/src/java/ht/GermainLescouflairSuy/gesBank/session/sessionBeanAdmin.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ht.GermainLescouflairSuy.gesBank.session;
+
+import javax.ejb.Stateful;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author MyPC
+ */
+@Stateful
+@LocalBean
+public class sessionBeanAdmin {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
