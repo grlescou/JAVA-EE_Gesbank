084241ccccbfe9a9eba39a774c85074018be5196
grlescou <gaetanrobert.lescouflair@esih.edu>
2015-09-01 21:57:43 -0400

    web default_templates

---
+++ TP_Final_JEE_GestionnaireDeCompteBancaires_Germain&Lescouflair&Suy-war/src/java/ht/GermainLescouflairSuy/gesBank/page/jsf/LoginMBean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ht.GermainLescouflairSuy.gesBank.page.jsf;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author MyPC
+ */
+@ManagedBean
+@SessionScoped
+public class LoginMBean {
+
+    /**
+     * Creates a new instance of LoginMBean
+     */
+    public LoginMBean() {
+    }
+    
+}
