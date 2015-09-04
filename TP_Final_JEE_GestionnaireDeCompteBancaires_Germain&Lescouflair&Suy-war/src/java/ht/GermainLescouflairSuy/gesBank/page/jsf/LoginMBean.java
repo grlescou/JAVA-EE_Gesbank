f360b171c8a8d28543191812efb17021066b3272
grlescou <gaetanrobert.lescouflair@esih.edu>
2015-09-04 11:49:03 -0400

    Merge origin/master
    Conflicts:
    	TP_Final_JEE_GestionnaireDeCompteBancaires_Germain&Lescouflair&Suy-war/nbproject/build-impl.xml
    	TP_Final_JEE_GestionnaireDeCompteBancaires_Germain&Lescouflair&Suy-war/nbproject/genfiles.properties
    	TP_Final_JEE_GestionnaireDeCompteBancaires_Germain&Lescouflair&Suy-war/nbproject/project.properties
    	TP_Final_JEE_GestionnaireDeCompteBancaires_Germain&Lescouflair&Suy-war/nbproject/project.xml
    
    Login and security

--- TP_Final_JEE_GestionnaireDeCompteBancaires_Germain&Lescouflair&Suy-war/src/java/ht/GermainLescouflairSuy/gesBank/page/jsf/LoginMBean.java
+++ TP_Final_JEE_GestionnaireDeCompteBancaires_Germain&Lescouflair&Suy-war/src/java/ht/GermainLescouflairSuy/gesBank/page/jsf/LoginMBean.java
@@ -6,9 +6,25 @@
 package ht.GermainLescouflairSuy.gesBank.page.jsf;
 
 import ht.GermainLescouflairSuy.gesBank.entite.Client;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.faces.application.ConfigurableNavigationHandler;
+import javax.faces.application.FacesMessage;
+import javax.faces.application.NavigationHandler;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ComponentSystemEvent;
+import org.primefaces.model.menu.DefaultMenuItem;
+import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.DefaultSubMenu;
+import org.primefaces.model.menu.MenuModel;
 
 /**
  *
@@ -26,8 +42,21 @@ public class LoginMBean {
     private String user;
     private String password ;
     private boolean connected = false;
+    private String Role="Admin" ;
+    private String PageRole;
+    //ExternalContext context = FacesContext.getCurrentInstance().getExternalContext(); 
+    FacesContext fc = FacesContext.getCurrentInstance();
+    ConfigurableNavigationHandler nav;
+   //NavigationHandler nh ;
+   
+    private MenuModel model;
     
     public LoginMBean() {
+         //  nh = fc.getApplication().getNavigationHandler();
+  nav = (ConfigurableNavigationHandler) fc.getApplication().getNavigationHandler();
+           client = new Client();
+           client.setNom("Lescouflair");
+           client.setPrenom("Gaetan Robert");
     }
 
     public Client getClient() {
@@ -62,17 +91,167 @@ public class LoginMBean {
         this.connected = connected;
     }
     
+    public void deconnexion(ActionEvent actionEvent){
+        this.connected = false;
+        nav.performNavigation("../login.xhtml?faces-redirect=true");
+    }
+
+    public String getRole() {
+        return Role;
+    }
+
+    public void setRole(String Role) {
+        this.Role = Role;
+    }
+
+    public String getPageRole() {
+        return PageRole;
+    }
+
+    public void setPageRole(String PageRole) {
+        this.PageRole = PageRole;
+    }
     
-    public void checkConnection(){
-        Client client =  null;
-        
-        if(true){
+       
+    
+     
+    public void addMessage(String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+    
+    
+    public String checkConnection(){
+        //Client client =  null;
+         connected = (user.equals("grl") && password.equals("1234"));
+        Role="Client";
+         if(connected){
+           
+               
+                return "client/MesComptes.xhtml?faces-redirect=true";
+           
             
         }
+         else{
+              return "login.xhtml?faces-redirect=true";
+         }
         
         
     }
     
     
+    
+    public void isUserConnected(ComponentSystemEvent event){
+        
+       PageRole = (String)event.getComponent().getAttributes().get("PageRole");
+    
+        if(!connected){
+         
+         
+         nav.performNavigation("login.xhtml?faces-redirect=true");
+         
+        }
+        else{
+            if(this.Role.equals(PageRole)== false)
+            {
+                nav.performNavigation("login.xhtml?faces-redirect=true");
+            }
+           nav.performNavigation("#");
+        }
+    }
+    
+    
+    
+ 
+    @PostConstruct
+    public void init() {
+        
+     model = new DefaultMenuModel();   
+     if(Role.equals("Client")){
+        
+        
+        
+         
+        //First submenu
+        DefaultSubMenu firstSubmenu = new DefaultSubMenu("Dynamic Submenu");
+         
+        DefaultMenuItem item = new DefaultMenuItem("Mes Comptes");
+        item.setUrl("http://www.primefaces.org");
+        item.setIcon("ui-icon-home");
+        firstSubmenu.addElement(item);
+         
+        model.addElement(firstSubmenu);
+         
+        //Second submenu
+        DefaultSubMenu secondSubmenu = new DefaultSubMenu("Dynamic Actions");
+ 
+        item = new DefaultMenuItem("Save");
+        item.setIcon("ui-icon-disk");
+        item.setCommand("#{menuView.save}");
+        //item.setUpdate("messages");
+        secondSubmenu.addElement(item);
+         
+        item = new DefaultMenuItem("Delete");
+        item.setIcon("ui-icon-close");
+        item.setCommand("#{menuView.delete}");
+        item.setAjax(false);
+        secondSubmenu.addElement(item);
+         
+        item = new DefaultMenuItem("Redirect");
+        item.setIcon("ui-icon-search");
+        item.setCommand("#{menuView.redirect}");
+        secondSubmenu.addElement(item);
+ 
+        model.addElement(secondSubmenu);
+        
+        }
+        
+         if(Role.equals("Admin")){
+        
+        
+        
+         
+        //First submenu
+        DefaultSubMenu firstSubmenu = new DefaultSubMenu("Dynamic Submenu");
+         
+        DefaultMenuItem item = new DefaultMenuItem("Liste des Comptes");
+        item.setUrl("http://www.primefaces.org");
+        item.setIcon("ui-icon-home");
+        firstSubmenu.addElement(item);
+         
+        model.addElement(firstSubmenu);
+         
+        //Second submenu
+        DefaultSubMenu secondSubmenu = new DefaultSubMenu("Dynamic Actions");
+ 
+        item = new DefaultMenuItem("Save");
+        item.setIcon("ui-icon-disk");
+        item.setCommand("#{menuView.save}");
+        //item.setUpdate("messages");
+        secondSubmenu.addElement(item);
+         
+        item = new DefaultMenuItem("Delete");
+        item.setIcon("ui-icon-close");
+        item.setCommand("#{menuView.delete}");
+        item.setAjax(false);
+        secondSubmenu.addElement(item);
+         
+        item = new DefaultMenuItem("Redirect");
+        item.setIcon("ui-icon-search");
+        item.setCommand("#{menuView.redirect}");
+        secondSubmenu.addElement(item);
+ 
+        model.addElement(secondSubmenu);
+        
+        }
+        
+       
+        
+    }
+ 
+    public MenuModel getModel() {
+        return model;
+    }   
+     
     
 }
